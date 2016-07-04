1e08e0e3f0f66f792db8cda2fcfea1b5f86a98fa
Francisco Calderon <fcalderonrodriguez@dons.usfca.edu>
2016-07-03 17:17:32 -0700

    updating files

--- stats/clt_unif.py
+++ stats/clt_unif.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
  
 N = 4 
-TRIALS = 500
+TRIALS = 2000
+
 
 X = []
 for i in range(TRIALS):
@@ -13,11 +14,22 @@ for i in range(TRIALS):
 
     X.append(means(data))
 
-plt.hist(X, bins=40, normed=1)
-
-mean = 0.5 
+mean = 0.5
 var = unifvar(0, 1) / N
-x_range = np.linspace(0, 1, len(X))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.hist(X, bins=40, normed=1)
+plt.text(.02, .9, '$N = %d$' % N, transform=ax.transAxes)
+plt.text(.02, 0.85, '$TRIALS = %d$' % TRIALS, transform=ax.transAxes)
+plt.text(.02, 0.80, 'mean($\\overline{X}$) = %f' % np.mean(X), transform=ax.transAxes)
+plt.text(.02, 0.75, 'var($\\overline{X}$) = %f' % np.var(X), transform=ax.transAxes)
+plt.text(.02, 0.7, 'var U($0,1$)/%d = %f' % (N, var), transform=ax.transAxes)
+
+plt.title("CLT Density Demo, sample mean of U(0,1) is $N(.5, \sigma^2/N)$")
+plt.xlabel("$\\overline{X}$", fontsize=16)
+plt.ylabel("Density", fontsize=16)
+
 Y = [normpdf(x, mean, sqrt(var)) for x in np.arange(0.0, 1.0, 0.005)]
 
 plt.plot(np.linspace(0.0, 1.0, len(Y)), Y, '-r')
